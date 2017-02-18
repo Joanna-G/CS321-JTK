ad66d16fb1b286eab4fcefb026bc27a9cced9424
Joanna <allinmythread@gmail.com>
2017-02-17 19:59:05 -0600

    Update Mailbox.java
    More modifications. Added some comments where discussion needs to be had about how the classes communicate. Not exactly sure how to handle a multi-line message, so for now I'm using a String ArrayList to hold lines.

--- ChargerMail/src/Mailbox.java
+++ ChargerMail/src/Mailbox.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Creates a mailbox
@@ -8,8 +9,48 @@ import java.util.ArrayList;
 
 public class Mailbox {
 
-	public Mailbox(String boxName) {
+	/**
+	 * Creates a mailbox
+	 * @param Account The current Account address
+	 * @param boxName Inbox, Sent, or Trash
+	 */
+	public Mailbox(String Account, String boxName) {
 		this.boxName = boxName;
+		currentAccount = Account;
+	}
+	
+	/**
+	 * Create a new message to send.
+	 */
+	public void newMessage() {
+		String toAddress;
+		ArrayList<String>messageText = new ArrayList<String>();
+		boolean done = false;
+		
+		input = new Scanner(System.in);
+		toAddress = input.nextLine();
+		input.next();
+		
+		// ArrayList holds lines of the message.
+		// Something needs to signal input is finished.
+		// When GUI is complete, we can wire it to the send button somehow.
+		while (!done) {
+			String text = input.nextLine();
+			input.next();
+			messageText.add(text);
+			if (input.nextLine().equals(""))
+				done = true;
+		}
+		
+		Message newMessage = new Message();
+		newMessage.setTo(toAddress);
+		newMessage.setFrom(currentAccount);
+		newMessage.setMessageText(messageText);
+		
+		// Some way to actually send the message.
+		// Like:
+		// currentAccount.send(newMessage); ??
+
 	}
 	
 	/**
@@ -25,21 +66,26 @@ public class Mailbox {
 	 * @param message
 	 */
 	public void removeMessage(Message message) {
-		/* some function that copies a message to the trash queue
-		Account.toTrash(message);
-		*/
+		/* some Account function that copies a message to Trash box.
+		 * Like:
+		 * if (currentAccount.getName() != Trash)
+		 * currentAccount.move(message, Trash); ??
+		 */
+	
 		messageQueue.remove(message);
 	}
 	
 	/**
-	* Empty the trash.
-	* @return true if emptied, false if not.
-	*/
+	 * Empty the trash.
+	 * @return true if emptied, false if not.
+	 */
 	public boolean emptyTrash() {
+		// Check to make sure this is the Trash
 		if (boxName.equals("Trash")) {
 			messageQueue.clear();
 			return true;
 		}
+		// If this is not the Trash, error popup or nothing happens ??
 		else
 			return false;
 	}
@@ -48,8 +94,13 @@ public class Mailbox {
 		return boxName;
 	}
 	
-	private ArrayList<Message> messageQueue = new ArrayList();
+	private ArrayList<Message> messageQueue = new ArrayList<Message>();
 	private String boxName;
-
+	private Scanner input;
+	
+	// Need some way to know what the current account is:
+	// private Account currentAccount;  ??
+	// For now, using a String.
+	private String currentAccount;
 	
 }
